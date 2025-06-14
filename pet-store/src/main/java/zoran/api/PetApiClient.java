62c03cdb84906836c7a3b4ed3eb659993b3a4ccc
NikolicZor <NikolicZor@sbsit.com>
2025-06-14 14:01:29 +0200

    Added PetApiClient.java

---
+++ pet-store/src/main/java/zoran/api/PetApiClient.java
@@ -0,0 +1,39 @@
+package zoran.api;
+
+import zoran.config.Config;
+import zoran.models.Pet;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class PetApiClient {
+    private final Config config;
+    
+    public PetApiClient() {
+        this.config = new Config();
+        RestAssured.baseURI = config.getBaseUrl();
+    }
+    
+    public Response addPet(Pet pet) {
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .header("api_key", config.getApiKey())
+                .body(pet)
+                .post("/pet");
+    }
+    
+    public Response getPetById(Long petId) {
+        return RestAssured.given()
+                .header("api_key", config.getApiKey())
+                .pathParam("petId", petId)
+                .get("/pet/{petId}");
+    }
+    
+    public Response updatePet(Pet pet) {
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .header("api_key", config.getApiKey())
+                .body(pet)
+                .put("/pet");
+    }
+}
---
+++ pet-store/src/main/java/zoran/models/Pet.java
@@ -0,0 +1,31 @@
+package zoran.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class Pet {
+    private Long id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls;
+    private List<Tag> tags;
+    private String status;
+}
+
+@Data
+@Builder
+class Category {
+    private Long id;
+    private String name;
+}
+
+@Data
+@Builder
+class Tag {
+    private Long id;
+    private String name;
+}
