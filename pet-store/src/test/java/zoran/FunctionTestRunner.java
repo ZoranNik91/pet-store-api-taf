5ea66648f885234ebbf1ec2fcfd77a7a699cdd74
NikolicZor <NikolicZor@sbsit.com>
2025-06-17 15:51:24 +0200

    added Allure fix 1

--- pet-store/src/test/java/zoran/FunctionTestRunner.java
+++ pet-store/src/test/java/zoran/FunctionTestRunner.java
@@ -1,6 +1,6 @@
 package zoran;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.*;
@@ -11,16 +11,17 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameters({
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "zoran.steps"),
         @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true"),
-        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-reports/cucumber.json, html:target/cucumber-reports/cucumber.html, rerun:target/rerun.txt"),
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, 
+            value = "pretty, " +
+                    "json:target/cucumber-reports/cucumber.json, " +
+                    "html:target/cucumber-reports/cucumber.html, " +
+                    "rerun:target/rerun.txt, " +
+                    "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"),
         @ConfigurationParameter(key = JUNIT_PLATFORM_NAMING_STRATEGY_PROPERTY_NAME, value = "long"),
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@petManagement")
+        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @wip")
 })
 public class FunctionTestRunner {
-    
-    // Main method to allow running from IntelliJ
-    public static void main(String[] args) {
-        // This will be called when running from IntelliJ
-        // The actual test execution is handled by JUnit 5 and Cucumber
-        // The @Suite annotation will take care of running the tests
-    }
+    // This class is used as a test suite for running Cucumber tests with JUnit 5
+    // The @Suite annotation will take care of running the tests
+    // No need for a main method when running with Maven or JUnit 5
 }
